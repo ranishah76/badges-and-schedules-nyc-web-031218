d091a169455bcde7f924d2ce204daf31516537dd
Rani Shah <rani76@gmail.com>
2018-02-26 21:44:53 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -6,7 +6,7 @@ attendees = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
 
 def batch_badge_creator(attendees)
   attendees.map do |attendee|
-     "Hello my name is #{attendee}."
+     "Hello, my name is #{attendee}."
 end
 end
 
@@ -25,4 +25,3 @@ def printer(attendees)
  end
 end
 
-  
