0e3fe64d1783d2ca71e3770b76b109bc22b0622f
Rani Shah <rani76@gmail.com>
2018-02-26 21:39:06 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,10 @@
-# Write your code here.
+def badge_maker(name)
+  puts "Hello, my name is #{name}"
+end
+
+attendees = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+
+def batch_badge_creator(attendees) 
+  attendees.all? { |name| puts "Hello my name is #{name}"}
+end 
+end 
